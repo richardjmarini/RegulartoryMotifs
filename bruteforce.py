52ede1dba606727d8e42c7a33bf7e76757c50ed8
Richard J. Marini <richardjmarini@gmail.com>
2018-06-18 15:40:29 -0400

    reversed logic

--- bruteforce.py
+++ bruteforce.py
@@ -10,26 +10,23 @@ def brute_force(sequences, k):
     # cycle through the list of all possible permutations
     # of possible offsets of length k across all the sequences
 
+    found= []
     for offset in product( range(len(sequences[0])-k), repeat= len(sequences) ):  
         
-
-        notfound= False
         for i in range(1, len(offset)):
 
             # if the k-mer of length k from the first sequence
-            # doesn't match the k-mer of length k from the next sequence
-            # then we didn't find a motify match between the two sequences
+            # match the k-mer of length k from the next sequence
+            # then we found a motif between the two sequences
 
-            if sequences[0][offset[0]:offset[0]+k] != sequences[i][offset[i]:offset[i]+k]:  
-                notfound= True
-                break 
+            if sequences[0][offset[0]:offset[0]+k] == sequences[i][offset[i]:offset[i]+k]:  
+                found.append((offset, sequences[0][offset[0]:offset[0]+k]))
 
         # if we did find a match between two k-mer's across any of the sequences
-        # then return the offset and the k-mer we found
+        # then return the offsets and the k-mer we found
+        if len(found): 
 
-        if not notfound:
-
-            return (offset, sequences[0][offset[0]:offset[0]+k])
+            return found 
 
 
 if __name__ == '__main__':
@@ -49,9 +46,10 @@ if __name__ == '__main__':
     # search time will grow expodentially so limit 
     # the sequences we're considering via start:end slice
 
-    found= brute_force(sequences[args.start:args.end], args.k)
-    if found:
-        (offset, pattern)= found
-        print "Found motif %s at offset %s" % (pattern, offset)
+    results= brute_force(sequences[args.start:args.end], args.k)
+    if len(results):
+        for result in results:
+            (offset, pattern)= result
+            print "Found motif %s at offset %s" % (pattern, offset)
     else:
         print "Could not find any motifs"
